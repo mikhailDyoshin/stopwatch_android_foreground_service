217c6705705707100ff225b1b6da13af9d123699
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2025-01-05 13:04:25 +0300

    Implemented service's logic and the logic that starts the service

--- app/src/main/java/com/example/stopwatchproject/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchproject/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.stopwatchproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.stopwatchproject.stopwatch.StopwatchService
 import com.example.stopwatchproject.ui.theme.StopwatchProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +27,10 @@ class MainActivity : ComponentActivity() {
             StopwatchProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        startService = {
+                            startService(Intent(this, StopwatchService::class.java))
+                        }
                     )
                 }
             }
@@ -31,17 +39,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier, startService: () -> Unit) {
+
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { startService() }) {
+            Text(text = "Start Service")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StopwatchProjectTheme {
-        Greeting("Android")
+        Greeting(startService = {})
     }
 }
